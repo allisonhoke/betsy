8b35a59a6889819c4eb472d50719dc637d47a581
Allison Hoke <allisonhoke@gmail.com>
2016-10-18 14:06:58 -0700

    updated relationships and db tables

--- db/migrate/20161018191600_create_reviews.rb
+++ db/migrate/20161018191600_create_reviews.rb
@@ -2,7 +2,8 @@ class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
       t.string :rating_description
-      t.integer :rating 
+      t.integer :rating
+      t.belongs_to :product
       t.timestamps null: false
     end
   end
