d4c82da472fab1d99837e6721c6cb3aecd579b62
Allison Hoke <allisonhoke@gmail.com>
2016-10-18 12:16:49 -0700

    scaffolded all models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :order_items
+  resources :categories
+  resources :reviews
+  resources :orders
+  resources :products
+  resources :merchants
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
