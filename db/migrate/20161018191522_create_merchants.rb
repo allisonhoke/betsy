41c0c2fe5a94492619d6daff791f523bb465fd96
Allison Hoke <allisonhoke@gmail.com>
2016-10-18 12:27:29 -0700

    added table and relationships to merchant

--- db/migrate/20161018191522_create_merchants.rb
+++ db/migrate/20161018191522_create_merchants.rb
@@ -1,7 +1,8 @@
 class CreateMerchants < ActiveRecord::Migration
   def change
     create_table :merchants do |t|
-
+      t.string :username, null: false
+      t.string :email, null: false
       t.timestamps null: false
     end
   end
