f844af43476ffc02e116b89e383cdf0a09acc8b1
Yuri Nakashima <yuri310@gmail.com>
2016-10-18 12:29:49 -0700

    added review tables

--- db/migrate/20161018191600_create_reviews.rb
+++ db/migrate/20161018191600_create_reviews.rb
@@ -1,7 +1,8 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-
+      t.string :rating_description
+      t.integer :rating 
       t.timestamps null: false
     end
   end
