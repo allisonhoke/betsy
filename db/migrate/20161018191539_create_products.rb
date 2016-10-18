aca6411d1baf01082d2295c657085e7f101930be
Yuri Nakashima <yuri310@gmail.com>
2016-10-18 14:26:12 -0700

    did migration tables for order, order_items, products

--- db/migrate/20161018191539_create_products.rb
+++ db/migrate/20161018191539_create_products.rb
@@ -1,7 +1,12 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
+      t.string :name
+      t.integer :price
+      t.string :description
+      t.string :photo_url
+      t.integer :stock
+      t.belongs_to :merchant_id
       t.timestamps null: false
     end
   end
--- db/migrate/20161018191549_create_orders.rb
+++ db/migrate/20161018191549_create_orders.rb
@@ -1,7 +1,8 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-
+      t.string :name
+      t.string :email
       t.timestamps null: false
     end
   end
