db98cd5efcab6a5ab1969bb3f3a523ccd78f2da4
Nicole Url <nicole.url.4@gmail.com>
2016-10-19 12:03:38 -0700

    Added validations and tests on review model

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @review = reviews(:correct_review)
+  end
+
+  test "Review must have a star rating" do
+    @review.rating = nil
+    assert_not @review.valid?
+  end
+
+  test "Review does not need a description" do
+    @review.rating_description = nil
+    assert @review.valid?
+  end
+
+  test "Review cannot have a 0 or 6 star rating" do
+    @review.rating = 0
+    review_two = reviews(:correct_review)
+    review_two.rating = 6
+
+    assert_not @review.valid?
+    assert_not review_two.valid?
+  end
+
+  test "Review can have a 1 or 5 star rating" do
+    @review.rating = 1
+    review_two = reviews(:correct_review)
+    review_two.rating = 5
+
+    assert @review.valid?
+    assert review_two.valid?
+  end
 end
