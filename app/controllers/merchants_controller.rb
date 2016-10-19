9fa95f062d45eac939c5fd9b9a0854b840172a04
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-19 14:27:54 -0700

    Added merchants controller Unit Tests; added error handling to merchants controller show method

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,8 +1,11 @@
 class MerchantsController < ApplicationController
 
   def show
-    @merchant = Merchant.find(params[:id])
-    @products = Product.where([:merchant_id] == @merchant.id)
+    begin
+      @merchant = Merchant.find(params[:id])
+      @products = Product.where([:merchant_id] == @merchant.id)
+    rescue ActiveRecord::RecordNotFound => err
+      render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+    end
   end
-
 end
