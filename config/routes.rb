59d17d8e793f0ce23931af6375abe7b72a94788e
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-19 11:57:18 -0700

    Adjusted merchant routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,12 @@ Rails.application.routes.draw do
 
   resources :orders, except: [:index]
 
-  resources :merchants do
+  resources :merchants, only: [:show] do
     resources :products, except: [:delete]
     resources :categories, except: [:edit, :update, :delete]
     resources :orders, only: [:index, :show]
   end
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
