8e04ed682ef8ac65f928506ec0a895a75c38e988
Yuri Nakashima <yuri310@gmail.com>
2016-10-19 11:50:30 -0700

    product validations added to model, validations testing added to product_test, yaml data added for products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "products with names are valid" do
+    product = products(:cat)
+    assert product.valid?
+  end
+
+  test "products without names are not valid" do
+    product = products(:dog)
+    assert_not product.nil?
+  end
+
+  test "products with a price greater than 0 are valid" do
+    product =  products(:narwal)
+    assert product.valid?
+  end
+
+  test "products without a price are not valid" do
+    product = products(:dog)
+    assert_not product.valid?
+  end
+
+  test "products with a price of 0 are not valid" do
+    product = products(:pig)
+    assert_not product.valid?
+  end
+
+  test "products with a price that are not an integer are not valid" do
+    product = products(:frog)
+    assert_not product.valid?
+  end
 end
