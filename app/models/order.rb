d727094878970ce9841f5160808f1aac6c9efeb7
Allison Hoke <allisonhoke@gmail.com>
2016-10-19 11:21:25 -0700

    removed unnecessary default stuff from yaml files. wrote method to find all order items by certain order_id and validation for order to test if has items

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ActiveRecord::Base
   has_many :order_items
   has_many :products, through: :order_items
+  validate :has_order_items?
+
+  def has_order_items?
+    OrderItem.items_in_order(self.id).length != 0
+  end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,9 @@
 class OrderItem < ActiveRecord::Base
 belongs_to :product
 belongs_to :order
+validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  def self.items_in_order(id)
+    OrderItem.where(order_id: id) #returns a collection of OrderItems with specified order_id
+  end
 end
