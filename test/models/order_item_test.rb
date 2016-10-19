063046f5671b6e3e2be69a6bc23868321f0be968
Allison Hoke <allisonhoke@gmail.com>
2016-10-19 11:57:33 -0700

    wrote tests for order item validations and items in order method. all tests passing

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,9 +1,51 @@
 require 'test_helper'
 
 class OrderItemTest < ActiveSupport::TestCase
+  test "Create an OrderItem with all valid data" do
+    item = order_items(:item_one)
+
+    assert item.save
+    assert item.valid?
+  end
+
+  test "Cannot create an order item with no quantity" do
+    item = OrderItem.new(product_id: 1, order_id: 1)
+
+    assert_not item.save
+    assert_not item.valid?
+    assert item.errors.keys.include?(:quantity), "quantity is not in the errors hash"
+  end
+
+  test "Cannot create an order item with quantity less than 1" do
+    item = OrderItem.new(quantity: 0, product_id: 1, order_id: 1)
+    another_item = OrderItem.new(quantity: -4, product_id: 1, order_id: 1)
+
+    assert_not item.save
+    assert_not item.valid?
+    assert item.errors.keys.include?(:quantity), "quantity is not in the errors hash"
+
+    assert_not another_item.save
+    assert_not another_item.valid?
+    assert another_item.errors.keys.include?(:quantity), "quantity is not in the errors hash"
+  end
+
+  test "Cannot create an order_item without an associated product_id" do
+    item = OrderItem.new(quantity: 1, order_id: 1)
+
+    assert_not item.save
+    assert_not item.valid?
+  end
+
+  test "Cannot create an order_item without an associated order_id" do
+    item = OrderItem.new(quantity: 1, product_id: 1)
+
+    assert_not item.save
+    assert_not item.valid?
+  end
+
   test "self.items_in_order should return correct number of OrderItems with passed in order_id" do
-    id = 1
-    other_id = 2
+    id = 1 # refers to yaml file
+    other_id = 2 # refers to yaml file
 
     assert_equal OrderItem.items_in_order(id).length, 2, "Returned the wrong number of order items"
     assert_equal OrderItem.items_in_order(other_id).length, 1, "Returned the wrong number of order items"
