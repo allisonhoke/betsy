e1e79b9c3202907ae2da50e55bcceb085c26f1ad
Allison Hoke <allisonhoke@gmail.com>
2016-10-20 16:37:13 -0700

    wrote method find_product to find associated product with order_item and wrote test: passing

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,9 @@ class OrderItem < ActiveRecord::Base
 belongs_to :product
 belongs_to :order
 validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  def find_product
+    product = Product.find(self.product_id)
+    return product
+  end
 end
