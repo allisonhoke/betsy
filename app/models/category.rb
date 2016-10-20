af79649053c7fed26cfe01783a83fc0ffb0bba1a
Nicole Url <nicole.url.4@gmail.com>
2016-10-19 19:49:39 -0700

    Decided to change the relationship between categories and products back to has_and_belongs_to_many

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-  has_many :products, through: :categories_products
+  has_and_belongs_to_many :products
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ActiveRecord::Base
 
   has_many :reviews
   belongs_to :user
-  has_many :products, through: :categories_products
+  has_and_belongs_to_many :categories
   has_many :order_items
   has_many :orders, through: :order_items
 end
