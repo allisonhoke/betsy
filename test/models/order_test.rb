ca446e7a9c7ef30cac0767b834fa64f039b51954
Allison Hoke <allisonhoke@gmail.com>
2016-10-20 16:36:36 -0700

    added total method in model and test for it. updated controller to use total in view

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -2,20 +2,24 @@ require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
   def setup
+    @product1 = Product.create(name: "cat", price: 1000)
+    @product2 = Product.create(name: "dog", price: 1234)
     @order = Order.new(name: "Allison", email: "allison@email.com")
 
-    @order.order_items << OrderItem.new(quantity: 1, product_id: 1)
-    @order.order_items << OrderItem.new(quantity: 1, product_id: 2)
+    @order.order_items << OrderItem.new(quantity: 1, product_id: @product1.id)
+    @order.order_items << OrderItem.new(quantity: 1, product_id: @product2.id)
     @order.save
   end
 
   test "Create an Order with all data" do
-
     assert @order.valid?
   end
 
-  test "items_in_order returns the items in the order" do
-
+  test "#items_in_order returns the items in the order" do
     assert_equal @order.items_in_order.length, 2, "Didn't return the correct number of items in this order"
+  end
+
+  test "#total returns the correct total of an order" do
+    assert_equal @order.total, 2234, "Didn't return the correct total"
   end
 end
