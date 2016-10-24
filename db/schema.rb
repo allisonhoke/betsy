07974f9aa0cb52c3cfd669edf53635a96819daa3
Allison Hoke <allisonhoke@gmail.com>
2016-10-24 11:18:06 -0700

    added migration to add status key to order model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021212415) do
+ActiveRecord::Schema.define(version: 20161024175907) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -52,8 +52,9 @@ ActiveRecord::Schema.define(version: 20161021212415) do
   create_table "orders", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.string   "status",     default: "in progress"
   end
 
   create_table "products", force: :cascade do |t|
