19f698b52f5d9735e8b219c131f31a9dc84e1780
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-24 15:19:13 -0700

    Added model method to find total revenue; test for method passing; added method call to contorller/instance variable to view

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -7,5 +7,8 @@ class MerchantsController < ApplicationController
     rescue ActiveRecord::RecordNotFound => err
       render file: "#{Rails.root}/public/404.html", layout: false, status: 404
     end
+
+    @merchant = Merchant.find(params[:id])
+    @merchant_oi_revenue = @merchant.find_all_order_items_revenue
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -14,4 +14,31 @@ class Merchant < ActiveRecord::Base
 
     return merchant
   end
+
+  def find_all_order_items_revenue
+    merchant = Merchant.find(self.id)
+    products = Product.where([:merchant_id] == merchant.id)
+
+    oi_quantity = []
+    product_price = []
+
+    products.each do |product|
+      order_items = OrderItem.where([:product_id] == product.id)
+      order_items.each do |item|
+
+        if product.id == item.product_id
+          oi_quantity.push(item.quantity)
+        end
+
+        if product.id == item.product_id
+          product_price.push(product.price)
+        end
+      end
+    end
+    sum = 0
+    oi_quantity.length.times do |i|
+      sum += oi_quantity[i-1] * product_price[i-1]
+    end
+    return sum
+  end
 end
