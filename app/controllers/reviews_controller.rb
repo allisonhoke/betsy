0a7293f8942bc1c3e20e41d23f515f2454bbf1e7
Nicole Url <nicole.url.4@gmail.com>
2016-10-23 19:08:22 -0700

    Finished first draft of reviews controller. Much more complicated than I thought.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,74 +1,70 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_review, only: [:show]
+  before_action :set_product_or_merchant, only: [:index, :show, :new]
 
-  # GET /reviews
-  # GET /reviews.json
   def index
-    @reviews = Review.all
+    @reviews = Review.all.where(product_id: params[:product_id])
   end
 
-  # GET /reviews/1
-  # GET /reviews/1.json
-  def show
-  end
 
-  # GET /reviews/new
+  def show; end
+
   def new
-    @review = Review.new
+    if @merchant.id == @product.merchant_id
+      render :review_error
+    else
+      @review = Review.new
+    end
   end
 
-  # GET /reviews/1/edit
   def edit
+    if @review.update(review_params)
+      redirect_to review_path(@review)
+    else
+      render :edit
+    end
   end
 
-  # POST /reviews
-  # POST /reviews.json
   def create
-    @review = Review.new(review_params)
-
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
-      else
-        format.html { render :new }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
+    if Review.create!(review_params)
+      if params[:product_id]
+        redirect_to product_reviews_path(params[:product_id])
+      elsif params[:merchant_id]
+        redirect_to merchant_product_reviews_path(params[:merchant_id])
       end
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /reviews/1
-  # PATCH/PUT /reviews/1.json
+
   def update
-    respond_to do |format|
-      if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
-        format.json { render :show, status: :ok, location: @review }
-      else
-        format.html { render :edit }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
-    end
+
   end
 
-  # DELETE /reviews/1
-  # DELETE /reviews/1.json
+
   def destroy
     @review.destroy
-    respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_review
       @review = Review.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def review_params
-      params.fetch(:review, {})
+      params.require(:review).permit(:rating_description, :rating, :product_id)
+    end
+
+    def set_product_or_merchant
+      begin
+        @product = Product.find(params[:product_id])
+        @merchant = Merchant.find(params[:merchant_id])
+      rescue
+        nil
+      end
     end
 end
