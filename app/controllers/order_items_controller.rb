047abfb006788f59552bb342041d8dc5e6a800cf
Allison Hoke <allisonhoke@gmail.com>
2016-10-25 12:25:58 -0700

    updated before action methods to call with proper actions

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,22 +1,21 @@
 class OrderItemsController < ApplicationController
-  before_action :set_order_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_order_item, only: [:update, :destroy]
 
   def update
-    @item = OrderItem.find(params[:id])
-    @item.update(order_item_params)
+    @order_item.update(order_item_params)
     render order_path # REVIEW: is this correct??
   end
 
   def destroy
-    @item = OrderItem.find(params[:id]).destroy
+    @order_item.destroy
 
     redirect_to order_path
   end
 
   private
-    # def set_order_item
-    #   @order_item = OrderItem.find(params[:id])
-    # end
+    def set_order_item
+      @order_item = OrderItem.find(params[:id])
+    end
 
     def order_item_params
       params.require(:order_items).permit(:quantity)
