47bcede98d769d583bf0e2504586f337c26b042a
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-24 18:53:34 -0700

    Adjusted controller to call current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :require_login
+
   def current_user
     @current_user ||= Merchant.find(session[:merchant_id]) if session[:merchant_id]
   end
 
   def require_login
-    if current_user.nil?
-      flash[:error] = "You must be logged in to view this section"
-      redirect_to root_path
-    end
+    current_user
+    # if current_user.nil?
+    #   flash[:error] = "You must be logged in to view this section"
+    #   redirect_to root_path
+    # end
   end
 
   def cart
