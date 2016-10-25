f1df3f2f2cc058e1459d7682c398dfddc44b4455
Yuri Nakashima <yuri310@gmail.com>
2016-10-25 15:26:56 -0700

    moved add to cart out of products controller, calling create order item in the products show view instead, adding create method to order items

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,6 +1,23 @@
 class OrderItemsController < ApplicationController
   before_action :set_order_item, only: [:update, :destroy]
-  before_action :cart, only: [:update]
+  before_action :cart, only: [:create, :update]
+
+  def new
+    product = Product.find(params[:id]).id
+    @order_item = OrderItem.new(product_id: product)
+  end
+
+
+  def create
+    id = Integer(params[:id])
+    @order_item = OrderItem.new(product_id: id, order_id: @cart.id)
+
+    if @order_item.save(order_item_parms)
+      redirect_to :order_path
+    else
+      render :failed_order_update
+    end
+  end
 
   def update
     @order_item.update(order_item_params)
