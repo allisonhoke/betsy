1bc311dfd5681c9d7f35d9c56c64eabe1851a6b1
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-24 23:14:43 -0700

    Only page's merchant's products show now

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -3,12 +3,16 @@ class MerchantsController < ApplicationController
   def show
     begin
       @merchant = Merchant.find(params[:id])
-      @products = Product.where([:merchant_id] == @merchant.id)
     rescue ActiveRecord::RecordNotFound => err
       render file: "#{Rails.root}/public/404.html", layout: false, status: 404
     end
 
-    @merchant = Merchant.find(params[:id])
+    begin
+      @products = Product.where(merchant_id: @merchant.id)
+    rescue ActiveRecord::RecordNotFound => err
+      render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+    end
+
     @merchant_oi_revenue = @merchant.find_all_order_items_revenue
   end
 end
