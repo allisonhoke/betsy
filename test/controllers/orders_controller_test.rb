b8df55ca75ac894128c6f492bf9c65c2323c2134
Allison Hoke <allisonhoke@gmail.com>
2016-10-25 11:53:23 -0700

    WIP controller tests

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -2,48 +2,24 @@ require 'test_helper'
 
 class OrdersControllerTest < ActionController::TestCase
   setup do
-    @order = orders(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:orders)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create order" do
-    assert_difference('Order.count') do
-      post :create, order: {  }
-    end
-
-    assert_redirected_to order_path(assigns(:order))
+    @order = orders(:order_one)
   end
 
   test "should show order" do
-    get :show, id: @order
+    get :show, id: @order.id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @order
+    get :edit, id: @order.id
     assert_response :success
   end
 
-  test "should update order" do
-    patch :update, id: @order, order: {  }
-    assert_redirected_to order_path(assigns(:order))
-  end
+  # test ""
 
-  test "should destroy order" do
-    assert_difference('Order.count', -1) do
-      delete :destroy, id: @order
-    end
+  # test "should update order" do
+  #   patch :update, id: @order, order: {  }
+  #   assert_redirected_to order_path(assigns(:order))
+  # end
 
-    assert_redirected_to orders_path
-  end
 end
