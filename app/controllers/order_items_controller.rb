691f4e254030d68ee2095d4321218a06d03f8b2b
Allison Hoke <allisonhoke@gmail.com>
2016-10-25 12:19:56 -0700

    deleted unnecessary view files. added to update method so order item will save and render cart view

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,17 +1,10 @@
 class OrderItemsController < ApplicationController
   before_action :set_order_item, only: [:show, :edit, :update, :destroy]
 
-  # def show
-  # end
-  #
-  #
-  # def edit
-  # end
-  #
-  #
   def update
     @item = OrderItem.find(params[:id])
-    # @item.quantity = params[:item][:qty] #added this line here
+    @item.update(order_item_params)
+    render order_path # REVIEW: is this correct??
   end
 
   def destroy
@@ -27,7 +20,7 @@ class OrderItemsController < ApplicationController
     # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    # def order_item_params
-    #   params.fetch(:order_item, {})
-    # end
+    def order_item_params
+      params.require(:order_items).permit(:quantity)
+    end
 end
