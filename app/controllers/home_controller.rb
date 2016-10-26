961f6e5abe3e88ffab15454feb7947f5791e79f7
Nicole Url <nicole.url.4@gmail.com>
2016-10-26 15:53:40 -0700

    Fixed a problem with duplicate products showing up on homepage if they have multiple 5 star reviews, dilly dallied with some layout on homepage as well

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   skip_before_action :require_login, only: [:index]
   def index
-    @popular = Review.where(rating: 5)
+    reviews = Review.all.where(rating: 5)
+    @popular = []
+    reviews.each do |review|
+      @popular << Product.find(review.product_id)
+    end
+    return @popular.uniq!.shuffle!
   end
 end
