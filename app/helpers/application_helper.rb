da6f206514f5d634ac8a9fc6a6c73601e31285d4
Nicole Url <nicole.url.4@gmail.com>
2016-10-26 15:44:16 -0700

    Application view helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,15 @@
 module ApplicationHelper
-  def merchant_only_button(wheretogo, button_words, button_power=:get)
+  def merchant_only_button(wheretogo, button_words, button_power, path_parameter="")
     if session[:merchant_id]
-      link_to new_category_path, method: button_power do
-        "<div class=\"button\">#{button_words}</div>".html_safe
+      link_to send(wheretogo, path_parameter), method: button_power do
+        "<div class=\"button\"><h4>#{button_words}</h4></div>".html_safe
       end
+    end
+  end
+
+  def link_button(wheretogo, button_words, button_power, path_parameter="")
+    link_to send(wheretogo, path_parameter), method: button_power do
+      "<div class=\"button\"><h4>#{button_words}<h4></div>".html_safe
     end
   end
 end
