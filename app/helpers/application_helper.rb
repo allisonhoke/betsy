3a68aa734ee4d7ca8bdf67b517c2db9fcd20cf48
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-26 15:47:17 -0700

    Added application helper to convert cents into dollar amounts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,16 @@ module ApplicationHelper
       end
     end
   end
+
+  # solution from http://stackoverflow.com/questions/7461290/convert-cents-into-dollar-string-in-ruby-without-use-of-bigdecimal
+  def add_decimal(cents)
+    pfx = [ '0.00', '0.0', '0.' ]
+    if(pfx[cents.to_s.length])
+      cents = pfx[cents.to_s.length] + cents.to_s
+    else
+      cents = cents.to_s.dup
+      cents[-2, 0] = '.'
+    end
+    return cents
+  end
 end
