7692e166712e0631dcc40839567b2fdf10dc70d0
Allison Hoke <allisonhoke@gmail.com>
2016-10-26 22:06:39 -0700

    updated create order item functionality to add to quantity in cart rather than create new order item if item already exists in cart - THIS COULD BE CLEANED UP

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -5,16 +5,42 @@ class OrderItemsController < ApplicationController
 
   def create
     product = Product.find(params[:product_id])
-    @order_item = OrderItem.new
-    @order_item.product_id = product.id
-    @order_item.order_id = @cart.id
 
-    @order_item.quantity = order_item_params(params)[:quantity]
-    if @order_item.save
-      redirect_to order_path
-    else
-      render :failed_order_update
+    counter = 0
+    @cart.order_items.each do |item|
+      if item.product_id == product.id
+        item.quantity += order_item_params(params)[:quantity].to_i
+        item.save
+        counter += 1
+      end
     end
+
+    if counter == 0
+      @order_item = OrderItem.new
+      @order_item.product_id = product.id
+      @order_item.order_id = @cart.id
+
+      @order_item.quantity = order_item_params(params)[:quantity]
+      unless @order_item.save
+        render :failed_order_update and return
+      end
+    end
+
+      redirect_to order_path
+
+
+    # end
+
+    # @order_item = OrderItem.new
+    # @order_item.product_id = product.id
+    # @order_item.order_id = @cart.id
+    #
+    # @order_item.quantity = order_item_params(params)[:quantity]
+    # if @order_item.save
+    #   redirect_to order_path
+    # else
+    #   render :failed_order_update
+    # end
   end
 
   def update
