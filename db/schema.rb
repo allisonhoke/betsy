ace9877d05bccee46f6d9a770101a43a342b84ca
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-26 22:58:51 -0700

    Migration and change reversal migration (as rollback errored)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024232734) do
+ActiveRecord::Schema.define(version: 20161027053752) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
