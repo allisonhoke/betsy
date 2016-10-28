8130d75a99c18d15acb7095d6f5e0b87b841ad17
Allison Hoke <allisonhoke@gmail.com>
2016-10-28 12:34:09 -0700

    added missing tests to product controller

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -12,6 +12,19 @@ class ProductsControllerTest < ActionController::TestCase
     assert_template :index
   end
 
+  test "should get products index through merchant path" do
+    session[:merchant_id] = @merchant.id
+    get :index,  :merchant_id => session[:merchant_id]
+    assert_response :success
+    assert_template :index
+  end
+
+  test "should get products index through category path" do
+    get :index,  :category_id => categories(:correct_category).id
+    assert_response :success
+    assert_template :index
+  end
+
   test "should get the new form for a new product for that merchant" do
     session[:merchant_id] = @merchant.id
     get :new, merchant_id: @merchant.id
@@ -41,6 +54,12 @@ class ProductsControllerTest < ActionController::TestCase
     session[:merchant_id] = @merchant.id
     patch :update, :merchant_id => session[:merchant_id], id: @product, product: {stock: 4}
      assert_redirected_to product_path(@product)
+  end
+
+  test "should create a valid product" do
+    session[:merchant_id] = @merchant.id
+    post :create, :merchant_id => session[:merchant_id], product: {name: "Product name", price: 1234, stock: 2}
+    assert_redirected_to merchant_path(@merchant)
   end
 
 end
