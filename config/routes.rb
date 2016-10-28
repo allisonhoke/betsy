b930a9995253d37a8452b14929fb4b75b4c2f107
Yuri Nakashima <yuri310@gmail.com>
2016-10-28 11:01:31 -0700

    updated merchants path on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   patch "products/:id/add_to_cart" => "products#add_to_cart"
 
 
-  resources :merchants, only: [:show] do
+  resources :merchants, only: [:show, :index] do
     get 'orders/:id' => 'orders#merchant_view', as: 'orders'
     resources :products, except: [:delete] do
       resources :reviews, only: [:show, :new, :create,:index]
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :delete]
     # resources :orders, only: [:merchant_view]
   end
-  
+
 
   # get 'merchants/:merchant_id/products/:id', to: 'products#show', as: 'merchant_product'
 
