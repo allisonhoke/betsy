4897ac6b059e0c9791890ff8f431c120ec69e1a0
Nicole Url <nicole.url.4@gmail.com>
2016-10-27 20:39:20 -0700

    Added one application helper test

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,12 +1,22 @@
 require 'test_helper'
+include ApplicationHelper
 
 class ApplicationHelperTest < ActionController::TestCase
-  # test "If a merchant is not logged in they cannot see their merchant-specific content" do
-  #   session[:merchant_id] = nil  # ensure no one is logged in
+  test "add_decimal should, you know, add a decimal" do
+    assert_equal "1.10", add_decimal(110)
+    assert_equal "1.00", add_decimal(100)
+    assert_equal "0.90", add_decimal(90)
+
+  end
+  # no idea how to test these
+  
+  # test "Merchant only button should only work if someone is logged in" do
+  #   session[:merchant_id] = nil
+  #   assert_not merchant_only_button.valid?
+  # end
   #
-  #   merchant_path(id: merchants(:teeny_merchant).id)
-  #   # if they are not logged in they cannot see the resource and are redirected to login.
-  #   assert_redirected_to root_path
-  #   assert_equal "You must be logged in first", flash[:error]
+  # test "link_button should work with 4 or 3 parameters" do
+  #   assert link_button("root_path","asdfsadf",:get).valid?
+  #   assert link_button("product_path","asdfasd",:get,products(:chair)).valid?
   # end
 end
